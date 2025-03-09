5488aac6657cbaa5f28cd7946048ff7d12445251
HperBayIsLive <72300826+TarunBali@users.noreply.github.com>
2025-03-10 01:09:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -134,12 +134,12 @@ window.addEventListener('load', () => {
 // Function to initialize projects and other functionality
 function initializeProjects() {
   const projects = [
-    { title: "LUCIFER", description: "A Voice Assistant for Windows 10", image: "Assets/project1.png" },
+    { title: "LUCIFER", description: "Lucifer is a sleek, voice-activated personal assistant designed to simplify your daily tasks with intuitive commands and seamless integration into your workflow.", image: "Assets/project1.png" },
     { title: "Wrath from the Shadows", description: "A shattered soul seeks retribution in the darkest corners, where the echoes of betrayal and revenge collide—but does he emerge victorious, or consumed by the very darkness he wields? ", image: "Assets/project2.jpg" },
     { title: "Celestia", description: "Digital Poster for a Sci-fi themed Cafe", image: "Assets/project3.jpg" },
-    { title: "DUZHDC-2", description: "The Second reel I made for the instagram page @DUZHDC", image: "Assets/project4.jpg" },
+    { title: "@DUZHDC", description: "A reel I made for the instagram page @DUZHDC", image: "Assets/project4.jpg" },
     { title: "Gesture Cursor Control", description: "A Python program that lets you control your cursor from fair using a webcam", image: "Assets/project5.jpg" },
-    { title: "DUZHDC-1", description: "The First reel I made for the instagram page @DUZHDC", image: "Assets/project6.jpg" },
+    { title: "Status of Acid Rain in India", description: "A Literature review delving into the Causes, Effects and Status of Acid Rain in India", image: "Assets/project6.jpg" },
   ];
 
   const projectFrame = document.querySelector('.project-frame');
